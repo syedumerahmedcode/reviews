9ce98882fb2ff87f55317da8df8d91b5986f2d07
Umer <80440412+syedumerahmedcode@users.noreply.github.com>
2022-07-09 15:57:44 +0200

    Feature: Created first entry in the array.

--- setup/app.js
+++ setup/app.js
@@ -1,5 +1,14 @@
+// local reviews data
 const reviews=[
-{},
+{
+  id: 1,
+  name: "susan smith",
+  job: "web developer",
+  img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    text:
+      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+},
 {},
 {},
 {}
