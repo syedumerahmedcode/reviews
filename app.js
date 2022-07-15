fe17cd17dedaacf0e5e3e942303120d142027fa4
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-07-15 21:49:02 +0200

    Feature: Implemented the logic for random button with the textContent Surprise Me.

--- app.js
+++ app.js
@@ -82,3 +82,9 @@ prevBtn.addEventListener("click", function () {
   }
   showPerson(currentItem);
 });
+
+// show random person
+randomBtn.addEventListener("click", function () {
+  currentItem = Math.floor(Math.random() * reviews.length);
+  showPerson(currentItem);
+});
